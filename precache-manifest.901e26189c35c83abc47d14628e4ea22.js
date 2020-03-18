c3f6182cea5fbb6587f8bf924ae95cb9dbed549b
apem <m_muflih@hotmail.com>
2020-03-19 01:32:46 +0700

    Updates

---
+++ precache-manifest.901e26189c35c83abc47d14628e4ea22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1535804501803de83f155c80a0ab8f",
+    "url": "/fable-gh-pages/index.html"
+  },
+  {
+    "revision": "1d945970568f873ca495",
+    "url": "/fable-gh-pages/static/js/2.7274d3e0.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/fable-gh-pages/static/js/2.7274d3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369f19f36ab61a86d6e1",
+    "url": "/fable-gh-pages/static/js/main.73458747.chunk.js"
+  },
+  {
+    "revision": "16b427ab84c398f9fd2c",
+    "url": "/fable-gh-pages/static/js/runtime-main.a5fd8c38.js"
+  }
+]);
